1de2d0eead7dd9288564ca4c833820b66b9181eb
Chris Polackal <cpolackal@Chriss-MacBook-Air.local>
2025-09-19 10:35:40 -0400

    comment changes

--- BurgerMan/src/game/Alist.java
+++ BurgerMan/src/game/Alist.java
@@ -1,5 +1,7 @@
 package game;
+
 /**
+ * Progress manager.
  * Interface for managing the player's progress in building a burger.
  * <p>
  * This interface defines the methods for generating the expected list of ingredients, 
@@ -8,7 +10,7 @@ package game;
  */
 interface Alist {
 	void generateExpectedList(int level);
-
+	/
 	void addPlayerIngredient(Rectangle ingredient);
 
 	boolean checkPlayerProgress();
